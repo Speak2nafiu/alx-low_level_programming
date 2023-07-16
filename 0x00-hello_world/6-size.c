ec9646d342f56e9bfca34dc7d18d00680b77ca3a
nafiu <speak2nafiu2016>
2023-07-16 01:14:41 -0700

    size task

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -10,6 +10,7 @@ int main(void)
 	printf("size of a int : 4 byte(s)\n");
 	printf("size of a long int : 4 byte(s)\n");
 	printf("size of a long long int : 8 byte(s)\n");
+	printf("size of a float : 4 byte(s)\n");
 	return (0);
 }
 

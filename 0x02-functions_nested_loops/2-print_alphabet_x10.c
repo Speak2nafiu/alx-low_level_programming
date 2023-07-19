b979cf5e336ae5e020a37af293aec6756aac9663
nafiu <speak2nafiu2016>
2023-07-19 11:43:07 -0700

    10 times alphabet

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * print_alphabet_x10 - check the code.
+ *
+ * Return: Always 0.
+ */
+int print_alphabet_x10(void)
+{
+	int x = 0;
+
+	while (x <= 10)
+	{
+		char letter = 'a';
+
+		while (letter <= 'z')
+		{
+			putchar(letter);
+			++letter;
+		}
+		++x;
+		putchar('\n');
+	}
+	return (0);
+}

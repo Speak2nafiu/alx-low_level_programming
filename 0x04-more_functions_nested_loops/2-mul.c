ecff2297fa12c08fc308fda3b2526430a46036ff
nafiu <speak2nafiu2016>
2023-07-21 19:48:10 -0700

    time

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * mul - times two numbers
+ * @a: number1
+ * @b: number2
+ *
+ * Return: Always 0.
+ */
+
+int mul(int a, int b)
+{
+
+	return (a * b);
+}

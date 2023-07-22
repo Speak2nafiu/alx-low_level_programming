2cc51c5708ec0e3a45853817a73854cef9eb8273
nafiu <speak2nafiu2016>
2023-07-21 17:36:05 -0700

    trial1

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - check the code.
+ *_isupper - returns 1 for capital and 0 for  lower case
+ * Return: Always 0
+ *
+ */
+
+int _isupper(int c)
+{
+	if (_isupper(c) > 0)
+	{
+		return (1);
+
+	}
+
+	else
+		{
+		return (0);
+
+		}
+
+	return (0);
+}
---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _isdigit - check the code
+ *
+ * c - input value
+ *
+ * Return: Always 0.
+ */
+
+
+int _isdigit(int c )
+{
+	if (_isdigit(c))
+	{
+		return (1);
+	}
+
+	else
+	{
+		return (0);
+	}
+
+	return (0);
+}

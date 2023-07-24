c8dc128b760f0f88226ae5fed9771e674741cd58
nafiu <speak2nafiu2016>
2023-07-24 12:01:17 -0700

    task A and B

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * reset_to_98  - reset n to 98
+ *
+ * Return: Always 0.
+ */
+
+void reset_to_98 (int a)
+{
+i
+	n* =  98;
+	return (0);
+}
---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * swap_int - swap two intergers
+ * @a : interger swap
+ * @b : interger swap
+ *
+ * Return: Always 0.
+ */
+
+void swap_int(int a,int b)
+{
+	temp = a;
+	a = b;
+	b = temp;
+	return (0);
+}

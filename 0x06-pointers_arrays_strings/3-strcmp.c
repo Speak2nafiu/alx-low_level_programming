ebaa060ed79b843a5141b169b88065fe15fe4cc0
speaknafiu <speaknafiu2nafiu2016>
2023-07-27 11:05:32 -0700

    compare

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * _strcmp - compares two strings
+ * @s1: first string
+ * @s2: second string
+ *
+ * Return: Always 0.
+ */
+
+int _strcmp(char *s1, char *s2)
+
+{
+	int result;
+
+	result = strcmp(s1, s2);
+
+	return (result);
+}

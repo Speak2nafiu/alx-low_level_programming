58a4223151c5c5e8b223f880e6a77bd6ff228857
speak2nafiu <speak2nafiu2016@gmail.com>
2023-07-31 15:22:39 -0700

    mem copy

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * _memcpy - copies a block of memory
+ * @dest: the address of memory to be copies to
+ * @src: the source memory to be copied
+ * @n: the size of the memory
+ *
+ * Return: Nothing.
+ */
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	memcpy(dest, src, n + 1);
+	return (dest);
+}

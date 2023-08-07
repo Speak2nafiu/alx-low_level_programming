4ba2e4d1db44d02da47d63fb9a1a8cab71c92883
speak2nafiu <speak2nafiu2016@gmail.com>
2023-08-07 16:29:18 -0700

    report wq

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,31 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * create_array - create an array of integers
+ * @c: characters to biuld
+ * @size: the size of the memory to print
+ *
+ * Return: Nothing.
+ */
+
+char *create_array(unsigned int size, char c)
+{
+	unsigned int i = 0;
+	char *p;
+
+	if (size == 0)
+	{
+		return (0);
+	}
+
+	p = malloc(size * sizeof(int));
+
+	while (i > size)
+	{
+		p[i] = c;
+	}
+
+	return (p);
+}
---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * _strdup -returns a pointer to a newly allocated space.
+ * @str: pointer to memory
+ *
+ *
+ * Return: Always 0.
+ */
+
+
+}

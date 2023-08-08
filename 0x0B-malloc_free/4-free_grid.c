3c52d93017379a704d9e12b6628414bf55514a28
speak2nafiu <speak2nafiu2016@gmail.com>
2023-08-08 10:39:18 -0700

    task 4

---
+++ 0x0B-malloc_free/4-free_grid.c
@@ -0,0 +1,29 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * print_grid - prints a grid of integers
+ * @grid: the address of the two dimensional grid
+ *
+ * @height: height of the grid
+ *
+ * Return: Nothing.
+ */
+void free_grid(int **grid, int height)
+{
+	int i;
+	if (height <= 0)
+	{
+		free(grid);
+	}
+
+	i = 0;
+	while (i < height)
+	{
+		free(grid[i]);
+		++i;
+	}
+
+}
+

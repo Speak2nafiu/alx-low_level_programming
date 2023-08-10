a60795481b0873df22ac8263c63a147fdf858cff
speak2nafiu <speak2nafiu2016@gmail.com>
2023-08-10 16:14:38 -0700

    task 1

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -2,39 +2,40 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-
 /**
  * string_nconcat - check the code
  *@s1: string input
  *@s2: string input
+ *@n: integer
  * Return: Always 0.
  */
-
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
 	int p;
 	int len;
-	void *dest;
+	char *dest;
 
 	if (n >= strlen(s2))
 	{
 		p = strlen(s2);
 	}
-	else
+	if (s1 == NULL)
 	{
-		p = n;
+		s1 = "";
 	}
-
+	if (s2 == NULL)
+	{
+		s2 = "";
+	}
+	else
+	{	p = n;
+	}
 	len = strlen(s1) + p + 1;
-
-	dest = malloc(len);
-	dest[len] = '/0';
-
+	dest = (char *) malloc(len);
 	if (dest == NULL)
 	{
 		return (NULL);
 	}
-
 	strcpy(dest, s1);
 	strncat(dest, s2, p);
 	return (dest);

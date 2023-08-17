d53d9ab6f3ecf34c6fb43c328425ed2142801065
speak2nafiu <speak2nafiu2016@gmail.com>
2023-08-17 14:02:44 -0700

    task4

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,40 @@
+#include <stdoi.h>
+#include "calc.h"
+
+/**
+ *main - defines all code
+ *op_add: adiition
+ *op_sub - subtraction
+ *Return: always 0.
+ *op_mul - multiplicatiocation
+ *op_div - division
+ *op_mod - modulus
+ */
+
+void main(void)
+{
+	int op_add(int a, int b)
+	{
+		return (a + b);
+	}
+
+	int op_sub(int a, int b)
+	{
+		return (a - b);
+	}
+
+	int op_mul(int a, int b)
+	{
+		return (a * b);
+	}
+
+	int op_div(int a, int b)
+	{
+		return (a / b);
+	}
+	int op_mod(int a, int b)
+	{
+		return (a % b);
+	}
+	return (void);
+}
